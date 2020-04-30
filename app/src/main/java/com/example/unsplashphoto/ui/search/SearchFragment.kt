57def54173bce13e5834685db4d2233a333d1c48
Anvar <anvarsv963@gmail.com>
2020-04-30 23:58:47 +0300

    add search layout

--- app/src/main/java/com/example/unsplashphoto/ui/search/SearchFragment.kt
+++ app/src/main/java/com/example/unsplashphoto/ui/search/SearchFragment.kt
@@ -2,14 +2,17 @@ package com.example.unsplashphoto.ui.search
 
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.widget.SearchView
+import com.example.unsplashphoto.MainActivity
 
 import com.example.unsplashphoto.R
+import kotlinx.android.synthetic.main.search_fragment.*
 
 class SearchFragment : Fragment() {
+
+    private lateinit var searchView: SearchView
 
     companion object {
         fun newInstance() = SearchFragment()
@@ -24,9 +27,18 @@ class SearchFragment : Fragment() {
         return inflater.inflate(R.layout.search_fragment, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        (activity as MainActivity).setSupportActionBar(toolBar)
+    }
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(SearchViewModel::class.java)
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.search_picture_menu, menu)
+        searchView = menu.findItem(R.id.actionSearch) as SearchView
+    }
 }
