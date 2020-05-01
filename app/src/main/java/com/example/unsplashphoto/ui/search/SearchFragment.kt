d3e74a8f94fb56a4b29f9ca62be85d9323eae643
Anvar <anvarsv963@gmail.com>
2020-05-01 22:10:30 +0300

    fix crush in search fragment

--- app/src/main/java/com/example/unsplashphoto/ui/search/SearchFragment.kt
+++ app/src/main/java/com/example/unsplashphoto/ui/search/SearchFragment.kt
@@ -5,9 +5,11 @@ import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
 import android.widget.SearchView
+import androidx.lifecycle.ViewModelProvider
 import com.example.unsplashphoto.MainActivity
 
 import com.example.unsplashphoto.R
+import com.example.unsplashphoto.ui.UnsplashViewModel
 import kotlinx.android.synthetic.main.search_fragment.*
 
 class SearchFragment : Fragment() {
@@ -18,7 +20,7 @@ class SearchFragment : Fragment() {
         fun newInstance() = SearchFragment()
     }
 
-    private lateinit var viewModel: SearchViewModel
+    private lateinit var viewModel: UnsplashViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -34,11 +36,11 @@ class SearchFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(SearchViewModel::class.java)
+        viewModel = ViewModelProvider(this).get(UnsplashViewModel::class.java)
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.search_picture_menu, menu)
-        searchView = menu.findItem(R.id.actionSearch) as SearchView
+        searchView = menu.findItem(R.id.actionSearch).actionView as SearchView
     }
 }
