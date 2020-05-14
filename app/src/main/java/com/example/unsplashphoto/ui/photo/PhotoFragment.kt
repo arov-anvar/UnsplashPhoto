c0643506b6c933e2535328461d6aace493d6db06
Anvar <anvarsv963@gmail.com>
2020-05-14 23:05:12 +0300

    change

--- app/src/main/java/com/example/unsplashphoto/ui/photo/PhotoFragment.kt
+++ app/src/main/java/com/example/unsplashphoto/ui/photo/PhotoFragment.kt
@@ -5,7 +5,9 @@ import android.content.Context
 import android.content.ContextWrapper
 import android.graphics.Bitmap
 import android.graphics.drawable.Drawable
+import android.os.AsyncTask
 import android.os.Bundle
+import android.os.Environment
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -15,6 +17,9 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.resource.bitmap.DownsampleStrategy
+import com.bumptech.glide.request.RequestOptions
 import com.bumptech.glide.request.target.CustomTarget
 import com.bumptech.glide.request.transition.Transition
 import com.example.unsplashphoto.R
@@ -24,6 +29,7 @@ import kotlinx.android.synthetic.main.photo_fragment.*
 import java.io.File
 import java.io.FileOutputStream
 import java.io.IOException
+import java.lang.ref.WeakReference
 
 
 class PhotoFragment: Fragment() {
@@ -52,7 +58,7 @@ class PhotoFragment: Fragment() {
         val photoId = arguments!!.getString("photoId")
         if (photoId != null) {
             viewModel.getPhoto(photoId).observe(this@PhotoFragment, Observer {photo ->
-                urlPhoto = photo.urls.full
+                urlPhoto = photo.urls.small
                 fullPhotoImageView.loadImage(photo.urls.full)
                 countLikesTxt.text = "${photo.likes}"
                 imageScaleTxt.text = "${photo.width}x${photo.height}px"
@@ -62,10 +68,7 @@ class PhotoFragment: Fragment() {
         }
 
         downloadPhotoBtn.setOnClickListener {
-            Glide.with(requireContext())
-                .asBitmap()
-                .load(urlPhoto)
-                .into(GlideTarget("one.png"))
+            DownloadAndSaveImageTask(activity!!.applicationContext).execute(urlPhoto)
         }
     }
 
@@ -79,6 +82,7 @@ class PhotoFragment: Fragment() {
             try {
                 val cw =
                     ContextWrapper(requireContext())
+                val filePath = Environment.getExternalStorageDirectory()
                 val directory: File = cw.getDir("imageDir", Context.MODE_PRIVATE)
                 val file = File(directory, "UniqueFileName" + ".jpg")
                 if (!file.exists()) {
@@ -101,5 +105,38 @@ class PhotoFragment: Fragment() {
         }
 
         override fun onLoadCleared(placeholder: Drawable?) {}
+    }
+
+    inner class DownloadAndSaveImageTask(context: Context) : AsyncTask<String, Unit, Unit>() {
+        private var mContext: WeakReference<Context> = WeakReference(context)
+
+        override fun doInBackground(vararg params: String?) {
+            val url = params[0]
+            val requestOptions = RequestOptions().override(100)
+                .downsample(DownsampleStrategy.CENTER_INSIDE)
+                .skipMemoryCache(true)
+                .diskCacheStrategy(DiskCacheStrategy.NONE)
+
+            mContext.get()?.let {
+                val bitmap = Glide.with(it)
+                    .asBitmap()
+                    .load(url)
+                    .apply(requestOptions)
+                    .submit()
+                    .get()
+
+                try {
+                    var file = it.getDir("Images", Context.MODE_PRIVATE)
+                    file = File(file, "img1999.jpg")
+                    val out = FileOutputStream(file)
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 85, out)
+                    out.flush()
+                    out.close()
+                    Log.i("Seiggailion", "Image saved.")
+                } catch (e: Exception) {
+                    Log.i("Seiggailion", "Failed to save image.")
+                }
+            }
+        }
     }
 }
