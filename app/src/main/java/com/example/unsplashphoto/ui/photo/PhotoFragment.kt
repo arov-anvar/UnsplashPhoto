19b89cf7e182e4fabd9db1dd9a41b3af018b7a92
Anvar <anvarsv963@gmail.com>
2020-05-17 18:47:26 +0300

    change download image

--- app/src/main/java/com/example/unsplashphoto/ui/photo/PhotoFragment.kt
+++ app/src/main/java/com/example/unsplashphoto/ui/photo/PhotoFragment.kt
@@ -1,35 +1,16 @@
 package com.example.unsplashphoto.ui.photo
 
 import android.annotation.SuppressLint
-import android.content.Context
-import android.content.ContextWrapper
-import android.graphics.Bitmap
-import android.graphics.drawable.Drawable
-import android.os.AsyncTask
-import android.os.Bundle
-import android.os.Environment
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.bumptech.glide.Glide
-import com.bumptech.glide.load.engine.DiskCacheStrategy
-import com.bumptech.glide.load.resource.bitmap.DownsampleStrategy
-import com.bumptech.glide.request.RequestOptions
-import com.bumptech.glide.request.target.CustomTarget
-import com.bumptech.glide.request.transition.Transition
-import com.example.unsplashphoto.R
-import com.example.unsplashphoto.loadImage
 import com.example.unsplashphoto.ui.UnsplashViewModel
 import kotlinx.android.synthetic.main.photo_fragment.*
-import java.io.File
-import java.io.FileOutputStream
-import java.io.IOException
-import java.lang.ref.WeakReference
+import android.os.Bundle
+import com.example.unsplashphoto.*
 
 
 class PhotoFragment: Fragment() {
@@ -68,75 +49,8 @@ class PhotoFragment: Fragment() {
         }
 
         downloadPhotoBtn.setOnClickListener {
-            DownloadAndSaveImageTask(activity!!.applicationContext).execute(urlPhoto)
-        }
-    }
-
-    inner class GlideTarget(private val fileName: String): CustomTarget<Bitmap>(){
-
-        override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
-            saveImage(resource)
-        }
-
-        private fun saveImage(image: Bitmap) {
-            try {
-                val cw =
-                    ContextWrapper(requireContext())
-                val filePath = Environment.getExternalStorageDirectory()
-                val directory: File = cw.getDir("imageDir", Context.MODE_PRIVATE)
-                val file = File(directory, "UniqueFileName" + ".jpg")
-                if (!file.exists()) {
-                    Log.d("path", file.toString())
-                    var fos: FileOutputStream? = null
-                    try {
-                        fos = FileOutputStream(file)
-                        image.compress(Bitmap.CompressFormat.JPEG, 100, fos)
-                        fos!!.flush()
-                        fos.close()
-                    } catch (e: IOException) {
-                        e.printStackTrace()
-                    }
-                }
-                Toast.makeText(context, "Cool", Toast.LENGTH_SHORT).show()
-            } catch (error: Exception) {
-                Toast.makeText(context, "fuck ${error.toString()}", Toast.LENGTH_SHORT).show()
-                error.printStackTrace()
-            }
-        }
-
-        override fun onLoadCleared(placeholder: Drawable?) {}
-    }
-
-    inner class DownloadAndSaveImageTask(context: Context) : AsyncTask<String, Unit, Unit>() {
-        private var mContext: WeakReference<Context> = WeakReference(context)
-
-        override fun doInBackground(vararg params: String?) {
-            val url = params[0]
-            val requestOptions = RequestOptions().override(100)
-                .downsample(DownsampleStrategy.CENTER_INSIDE)
-                .skipMemoryCache(true)
-                .diskCacheStrategy(DiskCacheStrategy.NONE)
-
-            mContext.get()?.let {
-                val bitmap = Glide.with(it)
-                    .asBitmap()
-                    .load(url)
-                    .apply(requestOptions)
-                    .submit()
-                    .get()
-
-                try {
-                    var file = it.getDir("Images", Context.MODE_PRIVATE)
-                    file = File(file, "img1999.jpg")
-                    val out = FileOutputStream(file)
-                    bitmap.compress(Bitmap.CompressFormat.JPEG, 85, out)
-                    out.flush()
-                    out.close()
-                    Log.i("Seiggailion", "Image saved.")
-                } catch (e: Exception) {
-                    Log.i("Seiggailion", "Failed to save image.")
-                }
-            }
+            val str = "Изображение сохранено: " + fullPhotoImageView.toBitmap().saveToStorage(requireContext())
+            showToast(requireContext(), str)
         }
     }
 }
--- app/src/main/java/com/example/unsplashphoto/utils.kt
+++ app/src/main/java/com/example/unsplashphoto/utils.kt
@@ -1,6 +1,14 @@
 package com.example.unsplashphoto
 
+import android.content.ContentValues
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import android.os.Build
+import android.os.Environment
+import android.provider.MediaStore
 import android.widget.ImageView
+import android.widget.Toast
 import com.bumptech.glide.Glide
 import com.example.unsplashphoto.data.model.collections.GalleryResp
 import com.example.unsplashphoto.data.model.photos.PhotoResp
@@ -8,6 +16,12 @@ import com.example.unsplashphoto.data.model.search.Result
 import com.example.unsplashphoto.ui.collection.CurrentItem
 import com.example.unsplashphoto.ui.gallery.adapter.Collection
 import com.example.unsplashphoto.ui.search.adapter.Photo
+import java.io.File
+import java.io.IOException
+
+fun showToast(context: Context, str: String) {
+    Toast.makeText(context, str, Toast.LENGTH_SHORT).show()
+}
 
 fun ImageView.loadImage(url : String) {
     Glide.with(this)
@@ -45,3 +59,46 @@ fun List<PhotoResp>.mapToCollectionItems(): MutableList<CurrentItem> {
 
     return result
 }
+
+fun Bitmap.saveToStorage(context: Context): String {
+    val relativeLocation = Environment.DIRECTORY_PICTURES + File.pathSeparator + "PocketDeen"
+    val contentValues = ContentValues().apply {
+        put(MediaStore.MediaColumns.DISPLAY_NAME, System.currentTimeMillis().toString())
+        put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            put(MediaStore.MediaColumns.RELATIVE_PATH, relativeLocation)
+            put(MediaStore.MediaColumns.IS_PENDING, 1)
+        }
+    }
+
+    val resolver = context.contentResolver
+    val uri = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+
+    try {
+
+        uri?.let { uri ->
+            val stream = resolver.openOutputStream(uri)
+
+            stream?.let { stream ->
+                if (!this.compress(Bitmap.CompressFormat.JPEG, 80, stream)) {
+                    throw IOException("Failed to save bitmap.")
+                }
+            } ?: throw IOException("Failed to get output stream.")
+
+        } ?: throw IOException("Failed to create new MediaStore record")
+
+    } catch (e: IOException) {
+        if (uri != null) {
+            resolver.delete(uri, null, null)
+        }
+        throw IOException(e)
+    } finally {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q)
+            contentValues.put(MediaStore.MediaColumns.IS_PENDING, 0)
+    }
+
+    return uri.toString()
+}
+
+fun ImageView.toBitmap(): Bitmap = (this.drawable as BitmapDrawable).bitmap
+
